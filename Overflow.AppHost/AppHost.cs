09235c585af236a1397fb3a217f7a4007c2296af
ilfei <nsteven@163.com>
2025-10-31 20:40:41 +0800

    feat: 添加 Keycloak 支持
    - 在 AppHost.cs 中集成 Keycloak 配置
    - 在项目文件中添加 Aspire.Hosting.Keycloak 包引用

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,5 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var keycloak = builder
+	.AddKeycloak("keycloak", 6001)
+	.WithDataVolume("keycloak-data");
+
 builder
 	.Build()
 	.Run();
