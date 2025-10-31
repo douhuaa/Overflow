4169d117373485f29563b193bf66829ec9809326
ilfei <nsteven@163.com>
2025-10-31 21:15:55 +0800

    feat: 添加 QuestionService 项目
    - 新增 WeatherForecastController 以提供天气预报接口
    - 添加 WeatherForecast 模型以表示天气数据
    - 更新解决方案文件以包含 QuestionService 项目
    - 配置启动设置和日志记录

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,8 +1,14 @@
+using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var keycloak = builder
 	.AddKeycloak("keycloak", 6001)
 	.WithDataVolume("keycloak-data");
+
+var questionService = builder
+	.AddProject<QuestionService>("question-service")
+	.WithReference(keycloak)
+	.WaitFor(keycloak);
 
 builder
 	.Build()
