883d440451ecd01b307990a052d0378eb427173d
ilfei <nsteven@163.com>
2025-11-02 20:14:56 +0800

    feat: 添加答案功能及相关处理
    - 新增 Answer 模型及其数据库迁移
    - 实现答案的创建、更新、删除和接受功能
    - 更新问题模型以支持答案计数和导航属性

--- QuestionService/Data/QuestiongDbContext.cs
+++ QuestionService/Data/QuestiongDbContext.cs
@@ -8,6 +8,8 @@ public class QuestionDbContext(DbContextOptions options)
 {
 	public DbSet<Question> Questions { get; set; }
 	public DbSet<Tag> Tags { get; set; }
+
+	public DbSet<Answer>  Answers { get; set; }
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
 		base.OnModelCreating(modelBuilder);
---
+++ QuestionService/Models/Answer.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace QuestionService.Models;
+
+public class Answer
+{
+	[MaxLength(36)]
+	public string Id { get; set; } = Guid
+		.NewGuid()
+		.ToString();
+
+	[MaxLength(5000)]
+	public required string Content { get; set; }
+
+	[MaxLength(36)]
+	public required string UserId { get; set; }
+
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+	public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+	public bool Accepted { get; set; }
+	public int Votes { get; set; }
+
+	// navigation properties
+	[MaxLength(36)]
+	public required string QuestionId { get; set; }
+
+	[JsonIgnore]
+	public Question Question { get; set; } = null!;
+}
