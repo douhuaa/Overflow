092d728a5c577ec8a230712c4dced0456a796a4f
ilfei <nsteven@163.com>
2025-11-04 20:12:35 +0800

    feat: 添加 HeroUI 组件支持
    - 新增 hero.ts 和 Providers.tsx 文件
    - 在 layout.tsx 中集成 Providers 组件
    - 更新 globals.css 以支持 HeroUI
    - 修改 page.tsx 以展示 HeroUI 按钮

---
+++ webapp/src/components/Providers.tsx
@@ -0,0 +1,12 @@
+﻿'use client';
+
+import {ReactNode} from "react";
+import {HeroUIProvider} from "@heroui/react";
+
+export default function Providers({children}: { children: ReactNode }) {
+	return (
+		<HeroUIProvider>
+			{children}
+		</HeroUIProvider>
+	);
+}
