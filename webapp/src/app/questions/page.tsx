6fb82037bd49b9883bd20795f40c00323d8c8eee
ilfei <nsteven@163.com>
2025-11-05 20:07:37 +0800

    feat: 添加问题列表页面和问题卡片组件
    - 新增问题列表页面，展示所有问题
    - 添加问题卡片组件，显示问题详情和统计信息
    - 实现获取问题数据的功能

---
+++ webapp/src/app/questions/page.tsx
@@ -0,0 +1,15 @@
+﻿import {getQuestions} from "@/lib/actions/question-actions";
+import QuestionCard from "@/app/questions/question-card";
+
+export default async function QuestionsPage() {
+	const questions = await getQuestions();
+	return (
+		<>
+				{questions.map((question) => (
+					<div key={question.id} className='py-4 not-last:border-b w-full flex'>
+						<QuestionCard key={question.id} question={question}/>
+					</div>
+				))}
+		</>
+	);
+}
---
+++ webapp/src/lib/actions/question-actions.ts
@@ -0,0 +1,12 @@
+﻿import {Question} from "@/lib/types";
+
+const BASE_URL = 'http://localhost:8001';
+
+export async function getQuestions(tag?: string): Promise<Question[]> {
+	const url = new URL('/questions', BASE_URL);
+	if (tag) url.searchParams.set('tag', tag);
+
+	const response = await fetch(url);
+	if (!response.ok) throw new Error(response.statusText);
+	return await response.json();
+}
