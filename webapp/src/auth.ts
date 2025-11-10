e337a57a593a8a6548b63f12740755e2038bb713
ilfei <nsteven@163.com>
2025-11-10 22:58:41 +0800

    feat: 添加会话管理功能及相关类型定义
    - 集成会话认证，获取并展示用户会话信息
    - 添加 JWT 和 Session 的类型定义
    - 更新认证回调以存储访问令牌

--- webapp/src/auth.ts
+++ webapp/src/auth.ts
@@ -3,4 +3,19 @@ import Keycloak from "next-auth/providers/keycloak"
 
 export const {handlers, auth, signIn, signOut} = NextAuth({
 	providers: [Keycloak],
+	callbacks: {
+		async jwt({token, account}) {
+			if (account && account.access_token) {
+				token.accessToken = account.access_token
+			}
+			return token;
+		},
+		async session({session, token}) {
+			if (token) {
+				session.accessToken = token.accessToken
+			}
+
+			return session;
+		}
+	}
 })
