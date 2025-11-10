a93553d28113c6db41d285e4b3bdfe221c665898
ilfei <nsteven@163.com>
2025-11-10 18:22:26 +0800

    feat: 添加标签功能及相关组件
    - 在 QuestionHeader 中显示标签描述
    - 使用 Zustand 管理标签状态
    - 在 Providers 中加载标签数据

--- webapp/src/components/Providers.tsx
+++ webapp/src/components/Providers.tsx
@@ -1,13 +1,25 @@
 ﻿'use client';
 
-import {ReactNode} from "react";
+import {ReactNode, useEffect} from "react";
 import {HeroUIProvider, ToastProvider} from "@heroui/react";
 import {useRouter} from "next/navigation";
 import {ThemeProvider} from "next-themes";
+import {useTagStore} from "@/lib/hooks/useTagStore";
+import {getTags} from "@/lib/actions/tag-actions";
 
 
 export default function Providers({children}: { children: ReactNode }) {
 	const router = useRouter();
+	const setTags = useTagStore(state => state.setTags);
+
+	useEffect(() => {
+		const loadTags = async () => {
+			const {data: tags} = await getTags();
+			if (tags) setTags(tags);
+		}
+		void loadTags();
+	}, [setTags])
+	
 	return (
 		<HeroUIProvider navigate={router.push} className={'h-full flex flex-col'}>
 			<ToastProvider/>
