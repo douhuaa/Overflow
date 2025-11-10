18c258cd75c42d56c7c5163edcf0742aa74648b5
ilfei <nsteven@163.com>
2025-11-10 23:41:31 +0800

    feat: 添加刷新令牌功能及相关类型定义
    - 在 JWT 接口中添加 refreshToken 和 accessTokenExpires 字段
    - 实现 token 刷新逻辑以支持会话管理
    - 更新 Keycloak 提供者配置以包含所需参数

--- webapp/src/lib/types/next-auth.d.ts
+++ webapp/src/lib/types/next-auth.d.ts
@@ -12,5 +12,9 @@ declare module 'next-auth' {
 declare module 'next-auth/jwt' {
 	interface JWT {
 		accessToken: string;
+		refreshToken: string;
+		accessTokenExpires: number;
+		error?: string;
+		
 	}
 }
