43e91918867956a1f0520b4fe7887b8d227758fb
ilfei <nsteven@163.com>
2025-11-10 18:33:57 +0800

    feat: 优化获取标签的缓存策略
    - 修改获取标签的请求，增加缓存策略
    - 设置缓存强制更新和重验证时间为3600秒

--- webapp/src/lib/actions/tag-actions.ts
+++ webapp/src/lib/actions/tag-actions.ts
@@ -5,5 +5,5 @@ import {fetchClient} from "@/lib/api/fetchClient";
 import {Tag} from "@/lib/types";
 
 export async function getTags() {
-	return fetchClient<Tag[]>('/tags', 'GET')
+	return fetchClient<Tag[]>('/tags', 'GET', {cache: 'force-cache', next: {revalidate: 3600}})
 }
