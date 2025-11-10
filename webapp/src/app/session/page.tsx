b30bc1a0fbfb7ae12bcf2623feabf8c879ca3d2d
ilfei <nsteven@163.com>
2025-11-10 20:19:13 +0800

    feat: 添加认证测试按钮及相关功能
    - 新增 AuthTestButton 组件用于测试认证
    - 更新页面布局以包含认证测试按钮
    - 添加 testAuth 函数用于处理认证请求

--- webapp/src/app/session/page.tsx
+++ webapp/src/app/session/page.tsx
@@ -1,8 +1,18 @@
 ﻿import ErrorButtons from "@/app/session/errorButtons";
+import AuthTestButton from "@/app/session/authTestButton";
 
 
 export default function Page() {
 	return (
-		<ErrorButtons/>
+		<div className="px-6">
+			<div className='text-center'>
+				<h3 className='text-3xl'>Session dashboard</h3>
+			</div>
+
+			<div className='flex items-center gap-3 justify-center mt-6'>
+				<ErrorButtons/>
+				<AuthTestButton/>
+			</div>
+		</div>
 	);
 }
